e5c7a45f79e0b598fe40363d9b64ab3d173a9efd
ThunderStorm <93911563+TimDeveloper97@users.noreply.github.com>
2025-05-31 17:48:28 +0700

    create project maui

---
+++ src/maui-heater-manager/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace maui_heater_manager
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
