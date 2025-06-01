ac65b0497aeb4c7ce4d80762f4c3ce99f8bafd2b
ThunderStorm <93911563+TimDeveloper97@users.noreply.github.com>
2025-06-01 11:59:41 +0700

    update usage page

--- src/maui-heater-manager/Pages/UsagePage.xaml.cs
+++ src/maui-heater-manager/Pages/UsagePage.xaml.cs
@@ -1,9 +1,12 @@
+using maui_heater_manager.Domains;
+using maui_heater_manager.ViewModels;
+
 namespace maui_heater_manager.Pages;
 
-public partial class UsagePage : ContentPage
+public partial class UsagePage : BasePage<UsageViewModel>
 {
-	public UsagePage()
-	{
-		InitializeComponent();
-	}
+    public UsagePage(UsageViewModel viewModel) : base(viewModel)
+    {
+        InitializeComponent();
+    }
 }
--- src/maui-heater-manager/ViewModels/UsageViewModel.cs
+++ src/maui-heater-manager/ViewModels/UsageViewModel.cs
@@ -1,11 +1,72 @@
-﻿using maui_heater_manager.Domains;
+﻿using LiveChartsCore;
+using LiveChartsCore.Kernel.Sketches;
+using LiveChartsCore.SkiaSharpView;
+using LiveChartsCore.SkiaSharpView.Painting;
+using maui_heater_manager.Domains;
+using SkiaSharp;
 
 namespace maui_heater_manager.ViewModels;
 
 public partial class UsageViewModel : BaseViewModel
 {
+    public ISeries[] SeriesTrend { get; set; } = [
+        new ColumnSeries<double>
+        {
+            Values = [ 20, 50, 40, 20, 40, 30, 50 ],
+
+            // Defines the distance between every bars in the series
+            Padding = 10,
+
+            // Defines the max width a bar can have
+            MaxBarWidth = double.MaxValue,
+            
+        }
+    ];
+
+    // Các ngày trong tuần
+    public Axis[] XAxesTrend { get; set; } = [
+        new Axis
+        {
+            Labels = new[] { "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun" },
+            LabelsPaint = new SolidColorPaint(SKColors.Black)
+        }
+    ];
+
+    public double TotalTrend => ((ColumnSeries<double>)SeriesTrend[0]).Values?.Sum() ?? 0;
+
     public UsageViewModel(IServiceProvider serviceProvider) : base(serviceProvider)
     {
         Title = "UsageViewModel";
+
+        GenerateRandomPower();
+    }
+}
+
+public partial class UsageViewModel
+{
+    private readonly Random _random = new();
+
+    public ISeries[] SeriesPower { get; set; }
+
+    public ICartesianAxis[] XAxesPower { get; set; }
+
+    public double TotalPower { get; set; }
+
+    private void GenerateRandomPower()
+    {
+        var trend = 100;
+        var values = new List<int>();
+
+        for (var i = 0; i < 100; i++)
+        {
+            trend += _random.Next(-30, 50);
+            values.Add(trend);
+        }
+
+        TotalPower = values.Sum();
+        SeriesPower = [new ColumnSeries<int>(values)];
+        XAxesPower = [new Axis()];
+        XAxesPower[0].MinLimit = null;
+        XAxesPower[0].MaxLimit = null;
     }
 }
